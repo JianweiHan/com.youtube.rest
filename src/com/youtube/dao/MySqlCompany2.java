be49d4466a705547ffd3cce91f1c8a73b79317f7
jianwei <jhan0530@gmail.com>
2015-06-10 16:38:09 -0700

    EP4
    EP4

---
+++ src/com/youtube/dao/MySqlCompany2.java
@@ -0,0 +1,35 @@
+package com.youtube.dao;
+
+import javax.naming.*;
+import javax.sql.*;
+
+public class MySqlCompany2 {
+	
+	private static DataSource MySqlCom2 = null;
+	private static Context context = null;
+	
+	public static DataSource MySqlCom2Conn() throws Exception {
+		
+		if(MySqlCom2 != null){
+			return MySqlCom2;
+		}
+		
+		try{
+			
+			if(context == null)
+			{
+				context = new InitialContext();
+			}
+			
+			MySqlCom2 = (DataSource) context.lookup("jdbc/mysql_ds");
+			
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		return MySqlCom2;
+	}
+
+}
--- src/com/youtube/rest/status/V1_status.java
+++ src/com/youtube/rest/status/V1_status.java
@@ -1,7 +1,10 @@
 package com.youtube.rest.status;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+
+import java.sql.*;
+import com.youtube.dao.*;
 
 @Path("/v1/status")
 public class V1_status {
@@ -20,6 +23,42 @@ public class V1_status {
     public String returnVersion(){
 		return "<p>Version:</p>" + api_version;
 	}
+	
+	@Path("/database")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDatabaseStatus() throws Exception{
+		
+		PreparedStatement query =null;
+		String myString = null;
+		String returnString = null;
+		Connection conn = null;
+		
+		try{
+			
+			conn = MySqlCompany2.MySqlCom2Conn().getConnection();
+			query = conn.prepareStatement("select * from department");
+			ResultSet rs = query.executeQuery();
+			
+			while(rs.next())
+			{
+				myString = rs.getString("dept_name");
+			}
+			
+			query.close(); //close connection
+			
+			returnString = "<p>Database Status</p>"+ "<p>Depart Name: " + myString + "</p>";
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally{
+			if (conn != null) conn.close();
+		}
+		
+		return returnString;
+	}
+	
 
 
 }
