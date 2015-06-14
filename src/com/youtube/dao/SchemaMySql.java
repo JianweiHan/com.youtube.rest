693b448045e6adc690f2cf7afa882a9684f2357e
jianwei <jhan0530@gmail.com>
2015-06-14 13:13:33 -0700

    EP6_post
    POST

--- src/com/youtube/dao/SchemaMySql.java
+++ src/com/youtube/dao/SchemaMySql.java
@@ -6,38 +6,85 @@ import org.codehaus.jettison.json.JSONArray;
 
 import com.youtube.util.ToJSON;
 
-public class SchemaMySql extends MySqlCompany2{
-	
-	public JSONArray queryReturnbrandParts(String brand) throws Exception{
-		
+public class SchemaMySql extends MySqlCompany2 {
+
+	public int insertIntoPC_PARTS(String DEPT_NO, String DEPT_NAME,
+			String LOCATION)
+			throws Exception {
+
 		PreparedStatement query = null;
 		Connection conn = null;
-		
+
+		try {
+			/*
+			 * If this was a real application, you should do data validation
+			 * here before starting to insert data into the database.
+			 * 
+			 * Important: The primary key on PC_PARTS table will auto increment.
+			 * That means the PC_PARTS_PK column does not need to be apart of
+			 * the SQL insert query below.
+			 */
+			conn = mysqlPcPartsConnection();
+			query = conn
+					.prepareStatement("insert into department "
+							+ "(DEPT_NO, DEPT_NAME, LOCATION) "
+							+ "VALUES ( ?, ?, ? ) ");
+
+			query.setString(1, DEPT_NO);
+			query.setString(2, DEPT_NAME);
+			query.setString(3, LOCATION);
+/*
+			// PC_PARTS_AVAIL is a number column, so we need to convert the
+			// String into a integer
+			int avilInt = Integer.parseInt(PC_PARTS_AVAIL);
+			query.setInt(4, avilInt);
+
+			query.setString(5, PC_PARTS_DESC);
+			
+*/
+			query.executeUpdate(); // note the new command for insert statement
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 500; // if a error occurs, return a 500
+		} finally {
+			if (conn != null)
+				conn.close();
+		}
+
+		return 200;
+	}
+
+	public JSONArray queryReturnbrandParts(String brand) throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+
 		ToJSON converter = new ToJSON();
 		JSONArray json = new JSONArray();
-		
-		try{
+
+		try {
 			conn = mysqlPcPartsConnection();
-			query = conn.prepareStatement("select * from department where UPPER(location) = ?");
+			query = conn
+					.prepareStatement("select * from department where UPPER(location) = ?");
 			query.setString(1, brand.toUpperCase());
 			ResultSet rs = query.executeQuery();
-			
+
 			json = converter.toJSONArray(rs);
-			query.close(); //close connection
+			query.close(); // close connection
 		}
-		
-		catch (SQLException sqlError){
+
+		catch (SQLException sqlError) {
 			sqlError.printStackTrace();
 			return json;
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return json;
+		} finally {
+			if (conn != null)
+				conn.close();
 		}
-		finally {
-			if (conn != null) conn.close();
-		}
-		
+
 		return json;
 	}
 
--- src/com/youtube/rest/inventory/V2_inventory.java
+++ src/com/youtube/rest/inventory/V2_inventory.java
@@ -1,5 +1,7 @@
 package com.youtube.rest.inventory;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
 import javax.ws.rs.PathParam;
@@ -8,7 +10,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.QueryParam;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
+
 import com.youtube.dao.SchemaMySql;
 
 @Path("/v2/inventory")
@@ -69,5 +73,53 @@ public class V2_inventory {
 		}
 		return Response.ok(returnString).build();
 	}
-
+	
+	
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON})
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addPcParts(String incomingData) throws Exception {
+		
+		String returnString = null;
+		//JSONArray jsonArray = new JSONArray(); //not needed
+		SchemaMySql dao = new SchemaMySql();
+		
+		try {
+			System.out.println("incomingData: " + incomingData);
+			
+			/*
+			 * ObjectMapper is from Jackson Processor framework
+			 * http://jackson.codehaus.org/
+			 * 
+			 * Using the readValue method, you can parse the json from the http request
+			 * and data bind it to a Java Class.
+			 */
+			ObjectMapper mapper = new ObjectMapper();  
+			ItemEntry itemEntry = mapper.readValue(incomingData, ItemEntry.class);
+			
+			int http_code = dao.insertIntoPC_PARTS(itemEntry.DEPT_NO, 
+													itemEntry.DEPT_NAME, 
+													itemEntry.LOCATION);
+			
+			if( http_code == 200 ) {
+				//returnString = jsonArray.toString();
+				returnString = "Item inserted";
+			} else {
+				return Response.status(500).entity("Unable to process Item").build();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+}
+class ItemEntry{
+	public String DEPT_NO;
+	public String DEPT_NAME;
+	public String LOCATION;
 }
